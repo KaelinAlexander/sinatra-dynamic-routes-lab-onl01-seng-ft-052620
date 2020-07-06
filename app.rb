52af91017b50d39546bccec0b4cabc9e4346c207
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-06 17:18:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   # get "/say/:number/:phrase" do
   #   @phrase = params[:phrase]
   #   @times = params[:number].to_i
-  #   @message = return @phrase * @times
+  #   @message = @phrase * @times
   #   "#{@message}"
   # end
   #
