9a3d78120806ad0db7c38e7316af8e1cc3f1051b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-06 17:07:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/:reverse_name" do
+    @name = params[:name].to_s
+    @reversed = @name.reverse
+    "Hello, #{@bye_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num_1 = params[:num1].to_i
+    @num_2 = params[:num2].to_i
+    @total = @num_1 * @num_2
+    "#{@total}"
+  end
+
 
 end
